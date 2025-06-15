45d9157b6f918427a6db92ed87df17251c6c56e7
dingyanxu <dingyanxu@dreame.tech>
2025-06-15 11:33:09 +0800

    add: chapter6/threadsafe_queue_accurate

---
+++ chapter6/threadsafe_queue_accurate/src/main.cpp
@@ -0,0 +1,109 @@
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <vector>
+#include <iostream>
+#include <thread>
+
+template <typename T>
+class threadsafe_queue {
+  struct node {
+    std::shared_ptr<T> data;
+    std::unique_ptr<node> next;
+  };
+
+ public:
+  threadsafe_queue() : head_(new node), tail_(head_.get()) {}
+  threadsafe_queue(const threadsafe_queue& other) = delete;
+  threadsafe_queue& operator=(const threadsafe_queue& other) = delete;
+  void push(T new_value) {
+    std::shared_ptr<T> new_data(std::make_shared<T>(std::move(new_value)));
+    std::unique_ptr<node> p(new node);
+    {
+      std::lock_guard<std::mutex> lock(tail_mutex_);
+      tail_->data = new_data;
+      node* const new_tail = p.get();
+      tail_->next = std::move(p);
+      tail_ = new_tail;
+    }
+    cond_.notify_one();
+  }
+  std::shared_ptr<T> try_pop() {
+    std::unique_ptr<node> old_head = try_pop_head();
+    return old_head ? old_head->data : std::shared_ptr<T>();
+  }
+  std::shared_ptr<T> wait_and_pop() {
+    std::unique_ptr<node> const old_head = wait_pop_head();
+    return old_head->data;
+  }
+  bool empty() {
+    std::lock_guard<std::mutex> lock(head_mutex_);
+    return head_.get() == get_tail();
+  }
+
+ private:
+  node* get_tail() {
+    std::lock_guard<std::mutex> lock(tail_mutex_);
+    return tail_;
+  }
+  std::unique_ptr<node> pop_head() {
+    std::unique_ptr<node> old_head = std::move(head_);
+    head_ = std::move(old_head->next);
+    return old_head;
+  }
+  std::unique_ptr<node> try_pop_head() {
+    std::lock_guard<std::mutex> lock(head_mutex_);
+    if (head_.get() == get_tail()) {
+      return nullptr;
+    }
+    return pop_head();
+  }
+  std::unique_ptr<node> wait_pop_head() {
+    std::unique_lock<std::mutex> lock(head_mutex_);
+    cond_.wait(lock, [&] { return head_.get() != get_tail(); });
+    return pop_head();
+  }
+
+ private:
+  mutable std::mutex head_mutex_;
+  mutable std::mutex tail_mutex_;
+  std::condition_variable cond_;
+  std::unique_ptr<node> head_;
+  node* tail_;  // 指向"虚位"节点(dummy node)
+};
+
+int main() {
+  threadsafe_queue<int> q;
+  
+  std::vector<std::thread> threads;
+  
+  threads.emplace_back([&q]() {
+    for(int i = 0; i < 5; i++) {
+      q.push(i);
+      std::cout << "Thread " << std::this_thread::get_id() << " push: " << i << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+  });
+  
+  threads.emplace_back([&q]() {
+    for(int i = 5; i < 10; i++) {
+      q.push(i);
+      std::cout << "Thread " << std::this_thread::get_id() << " push: " << i << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+  });
+  
+  threads.emplace_back([&q]() {
+    for(int i = 0; i < 10; i++) {
+      int value = *q.wait_and_pop();
+      std::cout << "Thread " << std::this_thread::get_id() << " pop: " << value << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+  });
+  
+  for(auto& t : threads) {
+    t.join();
+  }
+  
+  return 0;
+}
--- chapter6/threadsafe_queue_original/src/main.cpp
+++ chapter6/threadsafe_queue_original/src/main.cpp
@@ -71,30 +71,35 @@ public:
 int main() {
   threadsafe_queue<int> q;
   
-  auto push_func = [&q]() {
+  std::vector<std::thread> threads;
+  
+  threads.emplace_back([&q]() {
     for(int i = 0; i < 5; i++) {
       q.push(i);
       std::cout << "Thread " << std::this_thread::get_id() << " push: " << i << std::endl;
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-  };
-
-  auto pop_func = [&q]() {
-    for(int i = 0; i < 5; i++) {
-      int value;
-      q.wait_and_pop(value);
+  });
+  
+  threads.emplace_back([&q]() {
+    for(int i = 5; i < 10; i++) {
+      q.push(i);
+      std::cout << "Thread " << std::this_thread::get_id() << " push: " << i << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+  });
+  
+  threads.emplace_back([&q]() {
+    for(int i = 0; i < 10; i++) {
+      int value = *q.wait_and_pop();
       std::cout << "Thread " << std::this_thread::get_id() << " pop: " << value << std::endl;
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-  };
-
-  std::thread t1(push_func);
-  std::thread t2(pop_func);
-  std::thread t3(pop_func);
-
-  t1.join();
-  t2.join();
-  t3.join();
-
+  });
+  
+  for(auto& t : threads) {
+    t.join();
+  }
+  
   return 0;
 }
