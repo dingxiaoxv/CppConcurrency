ae64adcc546bf39d09864d61768b983780c9a347
dingyanxu <dingyanxu@dreame.tech>
2025-07-03 23:03:56 +0800

    fix: add run_pending_task

--- chapter9/thread_pool_wait/src/main.cpp
+++ chapter9/thread_pool_wait/src/main.cpp
@@ -1,4 +1,3 @@
-#include <atomic>
 #include <chrono>
 #include <exception>
 #include <future>
@@ -7,54 +6,7 @@
 #include <thread>
 #include <vector>
 
-#include "function_wrapper.h"
-#include "join_threads.h"
-#include "threadsafe_queue.h"
-
-class thread_pool {
- public:
-  thread_pool() : done_(false), joiner_(threads_) {
-    unsigned const thread_count = std::thread::hardware_concurrency();
-    try {
-      for (unsigned i = 0; i < thread_count; ++i) {
-        threads_.push_back(std::thread(&thread_pool::worker_thread, this));
-      }
-    } catch (...) {
-      done_ = true;
-      throw;
-    }
-  }
-
-  ~thread_pool() { done_ = true; }
-
-  template <typename FunctionType>
-  std::future<typename std::invoke_result<FunctionType>::type> submit(
-      FunctionType f) {
-    typedef typename std::invoke_result<FunctionType>::type result_type;
-    std::packaged_task<result_type()> task(std::move(f));
-    std::future<result_type> result(task.get_future());
-    work_queue_.push(std::move(task));
-    return result;
-  }
-
- private:
-  void worker_thread() {
-    while (!done_) {
-      function_wrapper task;
-      if (work_queue_.try_pop(task)) {
-        task();
-      } else {
-        std::this_thread::yield();
-      }
-    }
-  }
-
- private:
-  std::atomic<bool> done_;
-  threadsafe_queue<function_wrapper> work_queue_;
-  std::vector<std::thread> threads_;
-  join_threads joiner_;
-};
+#include "thread_pool.h"
 
 // 测试函数：计算斐波那契数列
 int fibonacci(int n) {
